7b238bce27e159492b46abac1b2a646bfad6f9e9
sunilpeocty <sshinde@peocty.com>
2016-10-28 19:22:12 +0530

    Role master added edit page

--- src/JobBundle/Controller/UserController.php
+++ src/JobBundle/Controller/UserController.php
@@ -30,4 +30,40 @@ class UserController extends Controller
                     'role'=>$role,
                 ));
     }
+    
+    /**
+     * @Route("/role/edit/{id}")
+     */
+    public function editAction($id)
+    {
+        //echo $id;
+        $em = $this->get('doctrine.orm.entity_manager');
+                  
+        $userManager = $this->get('fos_user.user_manager');
+        
+        $result = $userManager->findUserBy(array('id'=>$id));
+        //var_dump($result);
+        
+        return $this->render('default/userrole_edit.html.twig',array(
+                    'user'=>$result                 
+                ));
+        /*
+        // add flash message
+        $this->addFlash('notice','Your changes were saved!');
+        
+        $em = $this->get('doctrine.orm.entity_manager');
+                  
+        $userManager = $this->get('fos_user.user_manager');
+        
+        $result = $userManager->findUsers();
+		
+        $role = $this->getParameter('security.role_hierarchy.roles');
+        
+        return $this->render('default/user.html.twig',
+                array(
+                    'user'=>$result,
+                    'role'=>$role,
+                ));
+        */
+    }
 }
